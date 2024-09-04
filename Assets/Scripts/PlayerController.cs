22dfcdeab24a255fbf2dd6dc69b7aed260c27cab
LuisFelipe333 <luigifelipe333@gmail.com>
2024-09-04 02:02:54 -0600

    Add movement physics

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,23 +5,65 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
+    private Rigidbody2D rb;
+    public float moveSpeed = 50f;
+    public float jumpForce = 99999999f;
+    public bool isGrounded;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        //MOVE BY PIXELS
+        // if(Input.GetKey("left"))
+        // {
+        //     gameObject.transform.Translate(-50*Time.deltaTime, 0,0);
+        // }
 
-        if(Input.GetKey("left"))
+        // if(Input.GetKey("right"))
+        // {
+        //     gameObject.transform.Translate(50*Time.deltaTime, 0,0);
+        // }
+        //MOVE BY PIXELS
+
+
+        // Movimiento horizontal
+        float moveInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+
+        // Salto
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
-            gameObject.transform.Translate(-50*Time.deltaTime, 0,0);
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
 
-        if(Input.GetKey("right"))
+        // Detener movimiento horizontal cuando no se presiona ninguna tecla
+        if (Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKeyUp(KeyCode.LeftArrow))
         {
-            gameObject.transform.Translate(50*Time.deltaTime, 0,0);
+            rb.velocity = new Vector2(0, rb.velocity.y);
         }
     }
+
+     // Detectar si el personaje está en el suelo
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
+
+
 }
